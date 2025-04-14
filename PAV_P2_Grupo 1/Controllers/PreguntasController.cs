fe7939030f42b1228a9f0b05cf9e3b3dac16edfd
Joel30092005 <obando30092005@gmail.com>
2025-04-13 19:03:07 -0600

    Preguntas
    Desarrollo del apartado de preguntas

---
+++ PAV_P2_Grupo 1/Controllers/PreguntasController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using PAV_P2_Grupo_1.Data;
+using PAV_P2_Grupo_1.Models;
+
+namespace PAV_P2_Grupo_1.Controllers
+{
+    [Authorize]
+    public class PreguntasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PreguntasController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new Preguntas());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearPregunta(Preguntas pregunta)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                pregunta.IdUsuarioCreadorP = user.Id;
+                pregunta.Fecha = DateTime.Now;
+
+                _context.Preguntas.Add(pregunta);
+                await _context.SaveChangesAsync();
+
+                return Json(new { success = true });
+            }
+
+            return Json(new { success = false });
+        }
+    }
+}
+
